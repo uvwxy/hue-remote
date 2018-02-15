0cc4b16857aebc1993c28068475d8786f334f16e
Paul Smith <code@uvwxy.de>
2018-02-15 19:21:22 +0100

    Fix build errors

--- src/config.h
+++ src/config.h
@@ -1,5 +1,11 @@
-#define HUE_IP "SWOebegYQiDGTsVpp02QR4Rpz3EZcW04RyYMRtro"
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define HUEAPIKEY "SWOebegYQiDGTsVpp02QR4Rpz3EZcW04RyYMRtro"
 #define HUE_LIGHT 1
-#define HUE_APIKEY "192.168.1.183"
+#define HUEIP "192.168.1.183"
 #define WIFI_SSID "ssid"
 #define WIFI_PASS "pass"
+#define PORT 80
+
+#endif
